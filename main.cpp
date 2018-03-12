7fab652002cb39ffa49537b75d37ca4e43851d46
MarcinML <marcinliszcz@gmail.com>
2018-03-12 20:33:58 +0100

    'New rules about way how to collect letters each other'

--- main.cpp
+++ main.cpp
@@ -2,20 +2,79 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 
 
-std::string generateName()
+
+
+
+std::string generateName(int ile)
 {
     std::string name{};
+    char temp;
+    char tempForUppercase;
+
+    //Uppercase
     int wylosowana_liczba =( std::rand() % 26 ) + 65;
-    char temp=wylosowana_liczba;
-    name+=temp;
-    for(int i=0; i<10; ++i){
-    wylosowana_liczba =( std::rand() % 26 ) + 97;
+    name+=wylosowana_liczba;
+    tempForUppercase = wylosowana_liczba;
+
+    std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+
+
+
+    //Generate small letters
+    for(int i=0; i<ile-1; ++i){
+
+    do{
+            wylosowana_liczba =( std::rand() % 26 ) + 97;
+            temp=wylosowana_liczba;
+
+        }while((temp==name.back()) ||
+               temp =='q' ||
+               temp == 'y' ||
+               temp == 'x' ||
+               (temp == 'n' && name.back() == 'b') ||
+               (temp == 'x' && name.back() == 'n') ||
+               (temp == 'b' && name.back() == 'l') ||
+               (temp == 'g' && name.back() == 'p') ||
+               (temp == 'p' && name.back() == 'x') ||
+               (temp == 'f' && name.back() == 'p') ||
+               (temp == 'p' && name.back() == 'b') ||
+               (temp == 'v' && name.back() == 'n') ||
+               (temp == 'p' && name.back() == 'b') ||
+               (temp == 'b' && name.back() == 'c') ||
+               (temp == 'w' && name.back() == 'r') ||
+               (temp == 'b' && name.back() == 'g') ||
+               (temp == 'c' && name.back() == 'w') ||
+               (temp == 'w' && name.back() == 'c') ||
+               (temp == 'k' && name.back() == 'p') ||
+               (temp == 'p' && name.back() == 'k') ||
+               (temp == 'j' && name.back() == 'n') ||
+               (temp == 'n' && name.back() == 'j') ||
+               (temp == 'k' && name.back() == 'g') ||
+               (temp == 'g' && name.back() == 'k') ||
+               (temp == 'g' && name.back() == 'v') ||
+               (temp == 'v' && name.back() == 'g') ||
+               (temp == 'v' && name.back() == 'g') ||
+               (temp == 't' && name.back() == 'x') ||
+               (temp == 'x' && name.back() == 't') ||
+               (temp == 'j' && name.back() == 'c') ||
+               (temp == 'c' && name.back() == 'j') ||
+               (temp == 'k' && name.back() == 'd') ||
+               (temp == 'd' && name.back() == 'k') ||
+               (temp == 'd' && name.back() == 't') ||
+               (temp == 'c' && name.back() == 'l') ||
+               (temp == 'l' && name.back() == 'c') ||
+               (temp == 't' && name.back() == 'd') ||
+               (temp == 'p' && name.back() == 'd') ||
+               (temp == 'd' && name.back() == 'p') ||
+               (temp == 'c' && name.back() == 'b'));
+
     name+=wylosowana_liczba;
     }
 
-
+name.front() = tempForUppercase;
 
     return name;
 }
@@ -43,7 +102,7 @@ int main()
     {
 
         std::cout<<"To generate press 'g' and ENTER"<<std::endl;
-        std::cout<<generateName()<<std::endl;
+        std::cout<<generateName(ile)<<std::endl;
         std::cin>>a;
     }
 
